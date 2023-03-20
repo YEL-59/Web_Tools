7d23947d326192b5af269f9483ecf84e25776a0a
Md. Tofayel Islam <69710505+YEL-59@users.noreply.github.com>
2023-03-20 13:08:53 +0600

    form-react(data calling by usestate)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginForm from './components/LoginFirm/LoginForm';
+
+function App() {
+  return (
+    <div >
+       <LoginForm></LoginForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginFirm/login.js
@@ -0,0 +1,9 @@
+console.log(123)
+
+export function submitform() {
+    // console.log(123)
+    // let username = document.getElementById('username');
+    // console.log(username.val())
+    
+}
+
